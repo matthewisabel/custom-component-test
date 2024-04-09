adbe46c86abbf4380a2c2cce01c8b8b31a071160
Matthew Isabel <matthew.isabel@gmail.com>
2024-04-09 11:20:34 -0400

    Add supply chain

---
+++ src/supply_chain/SupplyChain.tsx
@@ -0,0 +1,59 @@
+import React, { FC, useCallback, useEffect } from 'react';
+import ReactFlow, {
+  addEdge,
+  MiniMap,
+  Controls,
+  Background,
+  useNodesState,
+  useEdgesState,
+} from 'reactflow';
+import { Retool } from '@tryretool/custom-component-support'
+import { nodes as initialNodes, edges as initialEdges } from './initial-elements';
+import CustomNode from './CustomNode';
+
+import 'reactflow/dist/style.css';
+import './overview.css';
+
+const nodeTypes = {
+  custom: CustomNode,
+};
+
+const minimapStyle = {
+  height: 120,
+};
+
+const onInit = (reacftFlowInstance) => console.log('flow loaded:', reactFlowInstance);
+
+export const SupplyChain: FC = () => {
+  const [journey] = Retool.useStateObject('journey', {nodes: [], edges: []});
+
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
+
+  useEffect(() => {
+    if (journey.nodes && journey.edges) {
+      setNodes(journey.nodes);
+      setEdges(journey.edges);
+    }
+  }, [journey, setNodes, setEdges]);
+
+  const onConnect = useCallback((params) => setEdges((eds) => addEdge(params, eds)), []);
+
+  return (
+    <ReactFlow
+      nodes={nodes}
+      edges={edges}
+      onNodesChange={onNodesChange}
+      onEdgesChange={onEdgesChange}
+      onConnect={onConnect}
+      onInit={onInit} // Ensure you define what you want to happen on init or remove if not needed.
+      fitView
+      attributionPosition="top-right"
+      // Ensure nodeTypes is correctly defined elsewhere in your code.
+      nodeTypes={nodeTypes}
+    >
+      <Controls />
+      <Background color="#aaa" gap={16} />
+    </ReactFlow>
+  );
+};
---
+++ src/supply_chain/initial-elements.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { MarkerType, Position } from 'reactflow';
+
+export const nodes = [
+  {
+    id: "placeholder-1",
+    type: "input",
+    data: { label: "Loading..." },
+    position: { x: 250, y: 0 },
+    style: { border: "1px dashed #999", color: "#999", backgroundColor: "rgba(0,0,0,0)" }
+  }
+];
+
+export const edges = [];
